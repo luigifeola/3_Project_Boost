e90f007f3935c2029e1d115ec97a437c03b4d9f4
luigi <feola.1772197@studenti.uniroma1.it>
2019-01-30 14:35:31 +0100

    Add_Level2

--- Assets/Scripts/Rocket.cs
+++ Assets/Scripts/Rocket.cs
@@ -26,6 +26,21 @@ public class Rocket : MonoBehaviour
         Rotate();
     }
 
+    void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "Friendly":
+                print("OK");
+                break;
+            case "Fuel":
+                print("Fuel");
+                break;
+            default:
+                print("Dead");
+                break;
+        }
+    }
 
     private void Rotate()
     {
